dc23fe5f70688ffe99c0e4d0158d6bf232fc3770
keshava kotri <keshav143420@gmail.com>
2019-08-21 14:42:19 +0530

    converted to class component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import './Card.css';
 
-const Card = () => {
-  return (
-    <div className="github-profile">
-      <img src="https://placehold.it/75" alt=''/>
-      <div className="info">
-        <div className="name">Name here...</div>
-        <div className="company">Company here...</div>
+class Card extends React.Component {
+  render() {
+    return (
+      <div className="github-profile">
+        <img src="https://placehold.it/75" alt='' />
+        <div className="info">
+          <div className="name">Name here...</div>
+          <div className="company">Company here...</div>
+        </div>
       </div>
-    </div>
-  )
+    );
+  }
 }
 
 export default Card;
