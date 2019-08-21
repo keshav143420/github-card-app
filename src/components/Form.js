e94396658ff58831499b992caa48f80bbb7af7a9
keshava kotri <keshav143420@gmail.com>
2019-08-21 15:24:13 +0530

    changed username to state

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,17 +1,25 @@
 import React from 'react';
 
 class Form extends React.Component {
+  state = {
+    userName: ''
+  }
   usernameInput = React.createRef();
   handleSubmit = (event) => {
     event.preventDefault();
-    console.log(this.usernameInput.current.value);
+    console.log(this.state.userName);
 
   }
   render() {
     return (
       <div>
         <form action="" onSubmit={this.handleSubmit}>
-          <input type="text" placeholder="Github username" ref={this.usernameInput} required/>
+          <input
+            type="text"
+            placeholder="Github username"
+            value={this.state.userName}
+            onChange={(event)=>this.setState({userName:event.target.value})}
+            required/>
           <button>Add Card</button>
         </form>
       </div>
