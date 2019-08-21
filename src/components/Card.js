5674571b8d32697eb13905279e7544d5c509ab83
keshava kotri <keshav143420@gmail.com>
2019-08-21 13:14:49 +0530

    Updated Card for github user profile

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,13 @@ import './Card.css';
 
 const Card = () => {
   return (
-    <div className="card">Github Card</div>
+    <div className="github-profile">
+      <img src="https://placehold.it/75" alt=''/>
+      <div className="info">
+        <div className="name">Name here...</div>
+        <div className="company">Company here...</div>
+      </div>
+    </div>
   )
 }
 
