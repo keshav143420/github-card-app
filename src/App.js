432da0ae3a64c49accbd45dde114a54417331f6f
keshava kotri <keshav143420@gmail.com>
2019-08-21 12:48:35 +0530

    Added hierarchy to components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Main from './components/Main';
 
 function App() {
   return (
-    <div>
-      Hello World
+    <div className="header">
+      <Main title="The Github Cards App" />
     </div>
   );
 }
