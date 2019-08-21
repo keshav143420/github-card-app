04dfd57270e16dce6bd3aff7de15d3368a2c591d
keshava kotri <keshav143420@gmail.com>
2019-08-21 15:02:52 +0530

    moved testData to Main

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,16 +1,10 @@
 import React from 'react';
 import Card from './Card';
 
-const testData = [
-  { name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook" },
-  { name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu" },
-  { name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook" },
-];
-
 const CardList = (props) => {
     return(
       <div>
-        {testData.map((profile,index) => <Card {...profile} key={index}/>)}
+        {props.profiles.map((profile,index) => <Card {...profile} key={index}/>)}
       </div>
     );
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -3,6 +3,12 @@ import './Main.css';
 import CardList from './CardList';
 import Form from './Form';
 
+const testData = [
+  { name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook" },
+  { name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu" },
+  { name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook" },
+];
+
 class Main extends React.Component {
   render() {
     return (
@@ -11,7 +17,7 @@ class Main extends React.Component {
           {this.props.title}
         </div>
         <Form />
-        <CardList />
+        <CardList profiles={testData} />
       </div>
     );
   }
