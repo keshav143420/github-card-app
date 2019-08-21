4e7583300089ab297c870c4a6178f68e9d5270e1
keshava kotri <keshav143420@gmail.com>
2019-08-21 15:17:44 +0530

    reading user input and displaying in console

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
 class Form extends React.Component {
+  usernameInput = React.createRef();
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.usernameInput.current.value);
+
+  }
   render() {
     return (
       <div>
-        <form action="">
-          <input type="text" placeholder="Github username" />
+        <form action="" onSubmit={this.handleSubmit}>
+          <input type="text" placeholder="Github username" ref={this.usernameInput} required/>
           <button>Add Card</button>
         </form>
       </div>
